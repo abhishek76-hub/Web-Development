ff534c858162fa6719f18de5762c9791a47404ff
abhishek76-hub <abhishekkandalkar8767@gmail.com>
2026-09-08 13:20:34 +0530

    Add Video 56

---
+++ Video 56/index.js
@@ -0,0 +1,48 @@
+console.log("Hey i am Js Tutorial")
+
+let age = 0;
+// let grace = 2;
+
+// age += grace
+// console.log(age)
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age * grace)
+// console.log(age / grace)
+// console.log(age ** grace)
+// console.log(age % grace)
+ 
+/* I 
+Am A Multiline
+comment */
+
+if (age == 18) {
+    console.log("You can drive");
+}
+
+else if (age == 0) {
+    console.log("Are you kidding?")
+}
+
+else if (age == 1) {
+    console.log("Are you again kidding?")
+}
+
+else {
+    console.log("You cannot drive");
+} 
+
+let a = 6;
+let b = 8;
+let c = a > b ? (a - b) : (b - a);
+
+/*
+translates to:
+if(a>b){
+    let c = a - b;
+}
+else {
+    let c = a - b;
+}
+
+*/
