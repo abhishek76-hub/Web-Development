13c52c9f1557667b4cc65791673402d71407c08d
abhishek76-hub <abhishekkandalkar8767@gmail.com>
2026-09-16 14:20:21 +0530

    Add Video 60

---
+++ Video 60/index.js
@@ -0,0 +1,26 @@
+console.log("This is strings tutorial")
+
+let a = "Abhi";
+console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+// console.log(a[4]);
+
+console.log(a.length);
+
+let real_name = "Abhi";
+let Friend = "Rohan";
+
+console.log("His name is " + real_name + " and his friends name is " + Friend)
+console.log(`His name is ${real_name} and his friends name is ${Friend}`)
+
+let b = "Shivam";
+
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.slice(1, 5))
+console.log(b.slice(1))
+console.log(b.replace("Sh" , "77"))
+console.log(b.concat(a,"Aishwariya","Rahul","Priya"))
